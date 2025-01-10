b1b9c01fceba14690ed8c42b6d5cd6b715acb8fc
andrei <andrei.kinder.170@gmail.com>
2025-01-10 14:27:31 +0500

    L-4: 4-Twig & Templates

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,16 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/', name: 'main')]
     public function homePage(): Response
     {
-        return new Response('myhomepage1');
+        $numberStarships =  40;
+        return $this->render('main/homepage.html.twig', ['numberStarships' => $numberStarships ]);
     }
 }
