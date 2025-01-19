362c1e35c9f659e2c8c22203dcc0cdc92e42a94e
andrei <andrei.kinder.170@gmail.com>
2025-01-19 14:59:39 +0500

    L-11: 11-Generating URLs

---
+++ src/Controller/StarshipApiController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\StarshipRepository;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+#[Route('/api/starships')]
+class StarshipApiController extends AbstractController
+{
+    #[Route('', name: 'starship', methods: ['GET'])]
+    public function getCollection(StarshipRepository $repository): JsonResponse
+    {
+        $starships = $repository->findAll();
+
+        return $this->json($starships);
+    }
+
+    #[Route('/{id<\d+>}', name: 'starship.single', methods: ['GET'])]
+    public function get(int $id, StarshipRepository $repository): JsonResponse
+    {
+        $starship = $repository->findById($id);
+        if (!$starship) {
+            throw $this->createNotFoundException('Starship not found');
+        }
+
+        return $this->json($starship);
+    }
+}
--- src/Controller/StarshipController.php
+++ src/Controller/StarshipController.php
@@ -3,29 +3,24 @@
 namespace App\Controller;
 
 use App\Repository\StarshipRepository;
-use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-#[Route('/api/starships')]
+
 class StarshipController extends AbstractController
 {
-    #[Route('', name: 'starship', methods: ['GET'])]
-    public function getCollection(StarshipRepository $repository): JsonResponse
-    {
-        $starships = $repository->findAll();
 
-        return $this->json($starships);
-    }
 
-    #[Route('/{id<\d+>}', name: 'starship.single', methods: ['GET'])]
-    public function get(int $id, StarshipRepository $repository): JsonResponse
+    #[Route('/starships/{id<\d+>}', name: 'app.starship.single', methods: ['GET'])]
+    public function get(int $id, StarshipRepository $repository): Response
     {
         $starship = $repository->findById($id);
         if (!$starship) {
             throw $this->createNotFoundException('Starship not found');
         }
 
-        return $this->json($starship);
+        return $this->render('starship/single.html.twig', [
+            'starship' => $starship,
+        ]);
     }
 }
